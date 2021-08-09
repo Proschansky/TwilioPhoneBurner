2659ed9eeb11b5345a60ec118c12e37e308c694c
auto-mausx <auto-mausx@gmail.com>
2021-08-08 19:08:09 -0600

    latest version

--- index.js
+++ index.js
@@ -19,12 +19,6 @@ const axios = require("axios");
 const { off } = require("process");
 const { request } = require("express");
 
-const officeIds = {
-  "+14705707952": "8976",
-  "+14076463947": "11214",
-  "+14709997211": "15611"
-};
-
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
@@ -55,72 +49,82 @@ const allowCrossDomain = function (req, res, next) {
     try {
       const twiml = new VoiceResponse();
     
-    twiml.say("We live bitches! Please say a short message about the nature of this call.");
+      twiml.say("We live bitches! Please say a short message about the nature of this call.");
+    
+      //twiml.dial('610-568-8542')
+      twiml.record({
+        timeout: 10,
+        maxLength: 10,
+        action: '/wait',
+        recordingStatusCallback: '/notify'
+      });
 
-    twiml.record({
-      timeout: 10,
-      maxLength: 10,
-      action: '/continue'
-    });
-
-    // Render the response as XML in reply to the webhook request
-    response.type("text/xml");
-    response.send(twiml.toString());
+      // Render the response as XML in reply to the webhook request
+      response.type("text/xml");
+      response.send(twiml.toString());
   } catch (e) {
     console.log("ERROR IN INCOMING CALL", e);
   }
 });
 
 
-app.post("/continue", (request, response) => {
+app.post("/notify", (request, response) => {
   
   // TODO: give option to accept or decline, figure out how to use gather inside of client.calls.create
-  const message = request.body.RecordingUrl
   try {
-    // by default play the recording
     const response = new VoiceResponse();
+    const message = request.body.RecordingUrl
+    console.log(request.body);
+
     client.calls
     .create({
-       twiml: "<Response><Play>" + message + "</Play><Gather input='dtmf' action='https://twilio-phone-burner-4dgbzfempa-uc.a.run.app/voice'><Say>Press 1 to accept this call</Say></Gather></Response>",
-       to: '+16105688542',
-       from: '+18327865719'
-     })
-    //response.play(message);
-    // TODO: handle call accept/reject statement
-    // If call is accepted, connect call
-    const gather = response.gather({
-      input: 'dtmf',
-      action: '/voice'
-    })
-
-    gather.say('Would you like to accept this call?')
+      //TODO: Need to forward to personal number through twilio number
+        twiml: "<Response><Play>" + message + "</Play><Gather action='https://twilio-phone-burner-4dgbzfempa-uc.a.run.app/connect' numDigits='1' input='dtmf'><Say>Press 1 to accept this call</Say></Gather></Response>",
+        //twiml: "<Response><Dial>610-568-8542</Dial></Response>", 
+        to: '+16105688542',
+        from: '+18327865719'
+      })
+    
+   
   } catch (e) {
     console.log("ERROR IN CALL CONTINUATION", e)
   };
 });
 
 // Create TwiML for outbound calls
-app.post("/voice", (request, response) => {
-  response = new VoiceResponse();
-
+app.post("/wait", (request, response) => {
   try {
-    if (request.body === '1') {
-      client.calls
-        .create({
-           //url: 'http://demo.twilio.com/docs/voice.xml',
-           to: '+16105688542',
-           from: '+18327865719'
-         })
-        .then(call => console.log(call.sid));
-      response.type("text/xml");
-      response.sendStatus(200);
+    // const response = new VoiceResponse();
+    const dial = response.dial();
+    console.log(request.body)
+    dial.enqueue({
+      waitUrl: 'music.xml'
+    }, 'waiting')
+
+  } catch (e) {
+    console.log("ERROR", e);
+  }
+});
+
+app.post("/music", (request, response) => {
+  // const response = new VoiceResponse
+  response.play('http://com.twilio.sounds.music.s3.amazonaws.com/MARKOVICHAMP-Borghestral.mp3');
+})
+
+app.post("/connect", (request, response) => {
+  // const response = new VoiceResponse();
+  try{
+    response.say("Please wait while we connect you")
+    if (request.body === "1") {
+      response.dial(request.body.Caller)
     } else {
+      response.say('Sorry, we could not complete your call')
       response.hangup();
     }
   } catch (e) {
     console.log("ERROR", e);
   }
-});
+})
 
 
 // app.post("/recording", upload.any(), (request, response) => {
